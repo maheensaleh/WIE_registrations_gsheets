bb413c255801477727d842a336176705b0071fa8
maheensaleh <salehmaheen@gmail.com>
2020-02-27 19:07:25 +0500

    home page/app added with sample page/app

--- google_data/google_data/settings.py
+++ google_data/google_data/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'sample'
+    'sample',
+    'home'
 ]
 
 MIDDLEWARE = [
--- google_data/google_data/urls.py
+++ google_data/google_data/urls.py
@@ -15,10 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
-from sample import views
 
 urlpatterns = [
+    path('', include("home.urls")),
     path('admin/', admin.site.urls),
-    path('', include("sample.urls")),
+    path('sample/', include("sample.urls")),
 
 ]
